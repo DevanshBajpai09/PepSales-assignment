c8df57998f45f8a662a73f43ecab1403eac64dba
Devansh bajpai <163446624+DevanshBajpai09@users.noreply.github.com>
2025-05-19 10:12:47 +0530

    Update route.js

--- app/api/notifications/route.js
+++ app/api/notifications/route.js
@@ -1,5 +1,5 @@
  
-import { prisma } from "../../../lib/prisma";
+import { prisma } from "../../../lib/prisma.js";
 import { sendNotificationToQueue } from "@/lib/rabbitmqProducer"; 
 import { NextResponse } from "next/server";
 
