7e4b137476f568bf5a78d204175a9886d8c33142
DevanshBajpai09 <devanshbajpai09@gmail.com>
2025-05-19 10:00:17 +0530

    Refactor Prisma client import paths and restore prisma.js for global instance management

--- app/api/notifications/route.js
+++ app/api/notifications/route.js
@@ -1,4 +1,5 @@
-import { prisma } from "@/lib/prisma";  
+ 
+import { prisma } from "../../../lib/prisma";
 import { sendNotificationToQueue } from "@/lib/rabbitmqProducer"; 
 import { NextResponse } from "next/server";
 
